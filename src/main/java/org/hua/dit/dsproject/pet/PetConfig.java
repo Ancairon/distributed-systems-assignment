50172280571c57ab10b539851ae9e2403f139fcd
losraio <evansraptis@gmail.com>
2021-12-18 18:56:09 +0200

    Τελείωσα τα κατοικίδια

---
+++ src/main/java/org/hua/dit/dsproject/pet/PetConfig.java
@@ -0,0 +1,22 @@
+package org.hua.dit.dsproject.pet;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class PetConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(PetRepository petRepository) {
+        return args -> {
+            Pet mydog = new Pet("Labrador Retriever", 'M', LocalDate.of(2010, 9, 8));
+
+            petRepository.saveAll(List.of(mydog));
+        };
+
+    }
+}
