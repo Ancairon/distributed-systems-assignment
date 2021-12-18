319b4ea1a944a1e3afc311dd4b2cd471ad5516bc
Fotis Voutsas <fotis@netdata.cloud>
2021-12-18 19:45:51 +0200

    finished users, format everything

--- src/main/java/org/hua/dit/dsproject/user/UserController.java
+++ src/main/java/org/hua/dit/dsproject/user/UserController.java
@@ -1,25 +1,37 @@
 package org.hua.dit.dsproject.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "/users/list")
-
 public class UserController {
 
-        private final UserService userService;
+    private final UserService userService;
 
-        @Autowired
-        public UserController(UserService userService) {
-            this.userService = userService;
-        }
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-        @GetMapping
-        public String getUser(){
-            return userService.helloWorld();
-        }
+    @GetMapping(path = "/users/list")
+    public List<User> getUser() {
+        return userService.listAllUsers();
+    }
 
+    @PostMapping(path = "/users/new")
+    public void newUser(@RequestBody User user) {
+        userService.newUser(user);
+    }
+
+    @DeleteMapping(path = "/users/delete/{id}")
+    public void deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping(path = "/users/modify/{id}")
+    public void modifyUser(@PathVariable("id") int id, @RequestParam(required = false) String username, @RequestParam(required = false) String password) {
+        userService.updateUser(id, username, password);
+    }
 }
